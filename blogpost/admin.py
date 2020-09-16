553da4f84785d80dc96e52db8c1e28766b8ae4e8
soma2108 <rin108sou@gmail.com>
2020-09-16 14:26:23 +0900

    first commit

--- blogpost/admin.py
+++ blogpost/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import SampleModel,BlogModel
+from .models import SampleModel, BlogModel
 
 # Register your models here.
-
 admin.site.register(SampleModel)
 admin.site.register(BlogModel)
